6e393cbc555a4159e9631a4ae031ea4ebaf68ab8
nitesh <niteshy089@gmail.com>
2020-03-03 00:18:22 +0530

    add todo app

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  public newitem= "";
+  public items=['item1','item2','item3','item4','item5','item6'];
+
+  addItem()
+  {
+    this.items.push(this.newitem)
+  }
+
+  delItem(item)
+{
+  this.items.splice(this.items.indexOf(item))
+}
+
+}
